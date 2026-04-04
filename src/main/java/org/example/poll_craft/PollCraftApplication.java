6da60eb08134b601a17ca0b736e7f4efa9e743a2
ferty460 <rostovcevvladimir460@gmail.com>
2026-04-04 19:49:19 +0300

    first commit

---
+++ src/main/java/org/example/poll_craft/PollCraftApplication.java
@@ -0,0 +1,13 @@
+package org.example.poll_craft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollCraftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PollCraftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/poll_craft/PollCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.poll_craft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollCraftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
